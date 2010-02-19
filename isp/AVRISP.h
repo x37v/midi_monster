6f8789ef33a22c70fc763c41770bd3eaf9508776
Alex Norman <x37v.alex@gmail.com>
2010-02-18 20:38:04 -0800

    ISP
    Slightly modified the LUFA ISP code to work for the monster, verified
    that we can program the tiny48 with it:
    
    avrdude -c avrisp2 -p t45 -P usb -e -U flash:w:../MIDI.hex -F
    
    my version of avrdude doesn't have the tiny48, so I called it the tiny45
    and have it ignoring the signature.  The chip programs and verifies,
    though it is bogus data for it..

---
+++ isp/AVRISP.h
@@ -0,0 +1,65 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2009.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+*/
+
+/*
+  Copyright 2009  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, and distribute this software
+  and its documentation for any purpose and without fee is hereby
+  granted, provided that the above copyright notice appear in all
+  copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  Header file for AVRISP.c.
+ */
+
+#ifndef _AVRISP_H_
+#define _AVRISP_H_
+
+	/* Includes: */
+		#include <avr/io.h>
+		#include <avr/wdt.h>
+		#include <avr/interrupt.h>
+		#include <avr/power.h>
+
+		#include "Descriptors.h"
+
+		#include <LUFA/Version.h>
+		#include <LUFA/Drivers/USB/USB.h>
+		
+		#if defined(ADC)
+			#include <LUFA/Drivers/Peripheral/ADC.h>
+		#endif
+		
+		#include "Lib/V2Protocol.h"
+
+	/* Function Prototypes: */
+		void SetupHardware(void);
+
+		void EVENT_USB_Device_Connect(void);
+		void EVENT_USB_Device_Disconnect(void);
+		void EVENT_USB_Device_ConfigurationChanged(void);
+
+		void Process_AVRISP_Commands(void);
+		
+#endif
